9b230cfb85c89cb284ed9fe10f7b3520fe6a7b68
gouravjeet <gouravjeet@gmail.com>
2014-07-29 00:30:59 -0400

    working API with commented code useful in future

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,9 +14,12 @@ angular.module('socialjusticeApp')
     $scope.sources = dataSource.query();
     $scope.data = {};
     $scope.dataTag = {};
+    $scope.dataset1={};
+    $scope.dataset2={};
+    $scope.dataset3={};
 
     $scope.$watch('dataTag',function(){
-        console.log($scope.dataTag);
+        //console.log($scope.dataTag);
     });
     $scope.tagObject={};
     $scope.saveTag=function(){
@@ -48,6 +51,27 @@ angular.module('socialjusticeApp')
             }
         }                
     };
+     // TagsModal info window
+    var tagsModal = $modal({scope: $scope, template: 'views/modal/tags.html', show: false});
+    $scope.makeModal = function(markerkey) {
+        console.log(markerkey);
+        
+        console.log($scope.data.length);
+        for(var index in $scope.data){
+            var temp=$scope.data[index];
+            console.log(temp);
+            for(var j in temp){
+                if(markerkey==temp[j].id){
+                    $scope.tagObject.nameTag=temp[j].name;
+                    $scope.tagObject.descriptionTag=temp[j].city;
+                    $scope.tagObject.id=temp[j].id;
+                    $scope.tagObject.addNewTag=temp[j].tags;
+                    tagsModal.$promise.then(tagsModal.show);
+                } 
+            }    
+        } 
+    };
+
     //Adding polygons
     $scope.singleModel = 1;
     $http.get('fake_data/test_neighbourhood.json').success(function(dataPoly){  
@@ -71,63 +95,9 @@ angular.module('socialjusticeApp')
         opacity: 1
     };
     
-    $scope.APIMArker=[ 
-            
-            {
-                'dataset': 'http://107.170.106.235/api-ds/11/', 
-                'id': 56487, 
-                
-                'name': 'MR SAM FOOD MARKET', 
-                'latitude': '42.667402000000003', 
-                'longitude': '-73.770546899999999', 
-                'street': '61 QUAIL ST', 
-                'city': 'ALBANY', 
-                'state': 'NY', 
-                'zipcode': '12206', 
-                'county': 'Albany', 
-                'field1': 'ALSAEDI GAMAL A', 
-                'tags': '', 
-                'field3': ''
-            }, 
-            {
-                'dataset': 'http://107.170.106.235/api-ds/11/', 
-                'id': 56488, 
-               
-                'name': 'WESTMERE NEWS & VARIETY', 
-                'latitude': '42.690869300000003', 
-                'longitude': '-73.867752999999993', 
-                'street': '1823 WESTERN AVE', 
-                'city': 'ALBANY', 
-                'state': 'NY', 
-                'zipcode': '12203', 
-                'county': 'Albany', 
-                'field1': 'WESTMERE NEWS INC', 
-                'tags': '', 
-                'field3': ''
-            }
-        ,
-            {
-                'dataset': 'http://107.170.106.235/api-ds/11/', 
-                'id': 56493, 
-                
-                'name': 'PRICE CHOPPER 138', 
-                'latitude': '42.754105500000001', 
-                'longitude': '-73.756456600000007', 
-                'street': '873 NEW LOUDON RD', 
-                'city': 'LATHAM', 
-                'state': 'NY', 
-                'zipcode': '12110', 
-                'county': 'Albany', 
-                'field1': 'PRICE CHOPPER OPERATING CO INC', 
-                'tags': '', 
-                'field3': ''
-            }
-        
-    ];
+    
 
-    var onMarkerClicked = function (marker) {
-        marker.showWindow = true;  
-    };
+   
     // _.each($scope.APIMArker, function (marker) {
     //     marker.closeClick = function () {
     //       marker.showWindow = false;
@@ -139,29 +109,96 @@ angular.module('socialjusticeApp')
     //     console.log(marker.showWindow);
     //     };
     // });
-    
+        //  _.each($scope.data, function (arr){
+        //     console.log(arr);
+        //      _.each(arr, function (marker) {
+        //         marker.closeClick = function () {
+        //           marker.showWindow = false;
+        //           $scope.$apply();
+        //         };
+        //         marker.onClicked = function () {
+        //             console.log("2");    
+        //             onMarkerClicked(marker);
+        //             $scope.$apply();
+        //         };
+        //     });
+        // });
+        //      for(var i in $scope.data){
+
+        //         for(var j in $scope.data[i]){
+                    
+        //         }
+        //      }
     $scope.onSelect = function(dataSourceId) {
         if($scope.data[dataSourceId] !== undefined) {
             $scope.data[dataSourceId] = undefined;
+             if(dataSourceId==1){
+                $scope.dataset1={};
+            }
+            else if(dataSourceId==11){
+                $scope.dataset2={};
+            } 
+            else if(dataSourceId==15){
+                $scope.dataset3=;
+            }
         }
-        else {
-            $scope.data[dataSourceId] = dataFeed.query({'dataSourceId':dataSourceId});
-             _.each($scope.data, function (arr){
-            console.log(arr);
-             _.each(arr, function (marker) {
-                marker.closeClick = function () {
-                  marker.showWindow = false;
-                  $scope.$apply();
-                };
-                marker.onClicked = function () {
-                    console.log("2");    
-                    onMarkerClicked(marker);
-                    $scope.$apply();
-                };
-            });
-        });
-            console.log($scope.data);
+        else 
+        {
+            $scope.data[dataSourceId] = dataFeed.query({'dataSourceId':dataSourceId});  
+            $scope.data[dataSourceId].$promise.then(function (result) {
+                $scope.data[dataSourceId] = result;
+                console.log($scope.data[dataSourceId]);
+                $scope.setValues(dataSourceId); 
+            });     
         }
+    };
+    
+    $scope.setValues=function(dataSourceId){
+        
+        if(dataSourceId==1){
+            $scope.dataset1=$scope.data[dataSourceId];
+        }
+        else if(dataSourceId==11){
+            $scope.dataset2=$scope.data[dataSourceId];
+        } 
+        else if(dataSourceId==15){
+            $scope.dataset3=$scope.data[dataSourceId];
+        }
+
+        
+        
+        console.log('Setting values');
+        console.log('Dataset1'+$scope.dataset1);
+        console.log('end');
+        console.log($scope.data[dataSourceId]);
+        
+        // var tempObj1=$scope.dataset1;
+        // for(var index in tempObj1){
+        //     tempObj1[index].showWindow=false;
+        //     console.log(tempObj1[index]);
+        // }
+        // $scope.dataset1=tempObj1;
+
+
+        // var onMarkerClicked = function (marker) {
+        //     marker.showWindow = true;  
+        // };
+        // _.each($scope.dataset1, function (marker) {
+              
+        //     marker.closeClick = function () {
+        //       marker.showWindow = false;
+        //       $scope.$apply();
+        //     };
+        //     marker.onClicked = function () {
+        //     onMarkerClicked(marker);
+        //     $scope.$apply();
+        //     //console.log(marker.showWindow);
+        //     };
+
+        // });
+        console.log($scope.dataset1);
+
+       
     };
    
     $scope.result = '';
@@ -220,50 +257,33 @@ angular.module('socialjusticeApp')
         'content': 'Add'
         }
     ];
-    $scope.showWindow=false;
+    $scope.showWindow=true;
     $scope.editTagEvents={
         options:{ 
             draggable:true},
         events:{ 
             // click:function(marker){
+            //     var markerKey=marker.key;
+            //     console.log(marker);
+            //    // $scope.resetInfo(markerKey);
             //     marker.showWindow = true;
             //     console.log('Marker clicked');    
             //     $scope.$apply();
             // },
              dblclick:function(marker){
                 console.log('dbl clicked');
-                marker.showWindow = false;
-                $scope.$apply();
+                var pos = marker.getPosition();
+                var markerKey=marker.key;
+                $scope.makeModal(markerKey);
+            }
+        }
+    };
 
-                var pos = marker.getPosition();
-                var markerKey=marker.key;
-                $scope.makeModal(markerKey);
-            }
-        }
-    };
-    $scope.markerEvents={
-        options:{ 
-            draggable:true},
-        events:{ 
-            click:function(marker){
-                marker.showWindow = true;
-                console.log('Marker clicked');
-                console.log(marker.key);    
-                $scope.$apply();
-            },
-             dblclick:function(marker){
-                console.log('dbl clicked');
-                var pos = marker.getPosition();
-                var markerKey=marker.key;
-                $scope.makeModal(markerKey);
-            }
-        }
-    };
+   
     $scope.infoWindowWithCustomClass= {
         options: {
             boxClass: 'custom-info-window'
-        },
-        show: true
+        }
     };
     $scope.hello=function(){
         alert("hello");
@@ -309,28 +329,77 @@ angular.module('socialjusticeApp')
         //     });
         // }    
     };
-   
-    // TagsModal info window
-    var tagsModal = $modal({scope: $scope, template: 'views/modal/tags.html', show: false});
-    $scope.makeModal = function(markerkey) {
-        console.log(markerkey);
-        
-        console.log($scope.data.length);
-        for(var index in $scope.data){
-            var temp=$scope.data[index];
-            console.log(temp);
-            for(var j in temp){
-                if(markerkey==temp[j].id){
-                    $scope.tagObject.nameTag=temp[j].name;
-                    $scope.tagObject.descriptionTag=temp[j].city;
-                    $scope.tagObject.id=temp[j].id;
-                    $scope.tagObject.addNewTag=temp[j].tags;
-                    tagsModal.$promise.then(tagsModal.show);
-                } 
-            }    
-        }
-        
-        
-    };
+    $scope.APIMArker=[ 
+            
+                {
+                'dataset': 'http://107.170.106.235/api-ds/11/', 
+                'id': 56487, 
+                
+                'name': 'MR SAM FOOD MARKET', 
+                'latitude': '42.667402000000003', 
+                'longitude': '-73.770546899999999', 
+                'street': '61 QUAIL ST', 
+                'city': 'ALBANY', 
+                'state': 'NY', 
+                'zipcode': '12206', 
+                'county': 'Albany', 
+                'field1': 'ALSAEDI GAMAL A', 
+                'tags': '', 
+                'field3': ''
+            } 
+            
+            ,
+                {
+                'dataset': 'http://107.170.106.235/api-ds/11/', 
+                'id': 56488, 
+                
+                'name': 'WESTMERE NEWS & VARIETY', 
+                'latitude': '42.690869300000003', 
+                'longitude': '-73.867752999999993', 
+                'street': '1823 WESTERN AVE', 
+                'city': 'ALBANY', 
+                'state': 'NY', 
+                'zipcode': '12203', 
+                'county': 'Albany', 
+                'field1': 'WESTMERE NEWS INC', 
+                'tags': '', 
+                'field3': ''
+            }
+        ,
+            {
+                'dataset': 'http://107.170.106.235/api-ds/11/', 
+                'id': 56493, 
+                
+                'name': 'PRICE CHOPPER 138', 
+                'latitude': '42.754105500000001', 
+                'longitude': '-73.756456600000007', 
+                'street': '873 NEW LOUDON RD', 
+                'city': 'LATHAM', 
+                'state': 'NY', 
+                'zipcode': '12110', 
+                'county': 'Albany', 
+                'field1': 'PRICE CHOPPER OPERATING CO INC', 
+                'tags': '', 
+                'field3': ''
+            }
+            
+            
+            
+    ];
+     $scope.resetInfo=function(markerKey){
+        for(var index in $scope.APIMArker){
+            for(var jindex in $scope.APIMArker[index]){
+                if(markerKey==$scope.APIMArker[index][jindex].id){
+                    console.log("called");
 
+                }
+                else{
+                    $scope.APIMArker[index][jindex].showWindow=false;
+                    console.log('reset info called');
+                }
+            }
+        }
+    };
+   
+   
 });
