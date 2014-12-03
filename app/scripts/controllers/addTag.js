cef863a2231ac752919f9eecf5309c98daeb6410
gouravjeet <gouravjeet@gmail.com>
2014-12-03 02:12:07 -0500

    cleaning

--- app/scripts/controllers/addTag.js
+++ app/scripts/controllers/addTag.js
@@ -3,8 +3,7 @@
 angular.module('socialjusticeApp')
 .controller('addTagCtrl',function($scope, $modal,$resource,dataEdit,djResource,downloadFile,config) {
 	// Pre-fetch an external template populated with a custom scope
-	$scope.damn='';
-	 $scope.damn='Gulf_Coast_Area_Geographies.xls';
+		$scope.damn='Gulf_Coast_Area_Geographies.xls';
 	var myOtherModal  = $modal({scope: $scope, template: 'views/modal/tags.html', show: false});
 	// Show when some event occurs (use $promise property to ensure the template has been loaded)
 	$scope.showModal = function() {
